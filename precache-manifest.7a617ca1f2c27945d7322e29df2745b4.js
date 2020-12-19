7e676e9f13e0089bdd2000082545fa44976ccdf0
MohamedZajith <mohamedzajith@gmail.com>
2020-12-19 17:22:55 +0530

    portfolio

---
+++ precache-manifest.7a617ca1f2c27945d7322e29df2745b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6552ca5d5bd08b2a9a3cdc03530db9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8897c0be833f1b439768",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5975a820bd56f9fdec6c",
+    "url": "/static/js/2.c822aed5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c822aed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8897c0be833f1b439768",
+    "url": "/static/js/main.8687cdba.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a119b83646d1250dd3c21f6cda108c83",
+    "url": "/static/media/contact_us.a119b836.svg"
+  },
+  {
+    "revision": "da110ed3a39d9d5fc08f4f42c9d8c3fd",
+    "url": "/static/media/mohamedzajith.da110ed3.jpeg"
+  },
+  {
+    "revision": "1a280523d375e9358d5229df34fc8e94",
+    "url": "/static/media/poppins-v9-latin-regular.1a280523.woff2"
+  },
+  {
+    "revision": "637889bdb57b027cd9bbf4b1289ccb7f",
+    "url": "/static/media/portfolio-1.637889bd.jpg"
+  }
+]);
